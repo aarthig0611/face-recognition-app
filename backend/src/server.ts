bc95c481e0eeebb93d53af9f47bc2931f6694c7b
aarthig0611 <aarthig0611@gmail.com>
2025-05-07 08:47:22 -0700

    Revert "Delete node_modules directory"

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -107,8 +107,6 @@ const loadLabeledImages = async (): Promise<faceapi.LabeledFaceDescriptors[]> =>
   return descriptors;
 };
 
-app.use(express.static(path.join(__dirname, 'build')));
-
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
